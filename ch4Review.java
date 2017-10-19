2c2d692ddbe75a8a25896899421dc95a7672bbc3
laurenjoycele <32201214+laurenjoycele@users.noreply.github.com>
2017-10-19 08:29:11 -0700

    Add files via upload

---
+++ ch4Review.java
@@ -0,0 +1,19 @@
+/* Lauren Le
+ * Period 1
+ * #2
+ */
+public class ch4Review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1; i<=10; i++)
+		{
+			for (int j=i; j<=i*10; j+=i)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
